0ecfb5e1167d34859259b50547b9e281f34fc997
Sandro Mesquita <39969611+Sandromesquita@users.noreply.github.com>
2021-10-26 18:38:49 -0300

    Create exemploTempo.h

---
+++ exemploTempo.h
@@ -0,0 +1,16 @@
+/*
+ * Arquivo exemplo da biblioteca Tempo.h
+ * Nome do Arquivo: exemploTempo
+ * Autor: Prof Sandro Mesquita
+ * Data: 22/10/2021
+ */
+ #include <Tempo.h>
+ tempo tempo1(13);
+ 
+void setup() {
+  
+}
+
+void loop() {
+  tempo1.contaTempo(2000);
+}
